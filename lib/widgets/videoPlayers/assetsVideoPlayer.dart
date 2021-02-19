ff507e471685efcfea92dd00816406ef0a348594
Jay <jay.shah@mail.codetrade.io>
2021-02-19 13:53:30 +0530

    [IMP] AssetsVideoplayer functionality Addded. [FIX] Gradle Version mismatch FIXED

--- lib/widgets/videoPlayers/assetsVideoPlayer.dart
+++ lib/widgets/videoPlayers/assetsVideoPlayer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+import '../videoPlayerWidget.dart';
 
 class AssetsVideoPlayer extends StatefulWidget {
   @override
@@ -6,8 +9,49 @@ class AssetsVideoPlayer extends StatefulWidget {
 }
 
 class _AssetsVideoPlayerState extends State<AssetsVideoPlayer> {
+  final asset = 'assets/video.mp4';
+  VideoPlayerController assetController;
+
+  @override
+  void initState() {
+    super.initState();
+    assetController = VideoPlayerController.asset(asset)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..setLooping(true)
+      ..initialize().then((value) => assetController.play());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    assetController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final isMuted = assetController.value.volume == 0;
+    return Column(
+      children: [
+        VideoPlayerWidget(
+          controller: assetController,
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        if (assetController != null && assetController.value.initialized)
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.red,
+            child: IconButton(
+              icon: Icon(
+                isMuted ? Icons.volume_mute : Icons.volume_up,
+              ),
+              onPressed: () => assetController.setVolume(isMuted ? 1 : 0),
+            ),
+          )
+      ],
+    );
   }
 }
