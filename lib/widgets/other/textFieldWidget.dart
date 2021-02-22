eea915a7c3fd6d662b5156305189ac520d998ff1
Jay <jshah200498@gmail.com>
2021-02-22 11:46:45 +0530

    [ADD]NetworkVideoPlayer functionality addded.

---
+++ lib/widgets/other/textFieldWidget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+
+  const TextFieldWidget({Key key, this.controller, this.hintText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 20,
+      ),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Colors.grey,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/videoPlayers/networkVideoPlayer.dart
+++ lib/widgets/videoPlayers/networkVideoPlayer.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_video_player/main.dart';
+import 'package:flutter_video_player/widgets/other/FloatingActionButtonWidget.dart';
+import 'package:flutter_video_player/widgets/other/textFieldWidget.dart';
+import 'package:flutter_video_player/widgets/videoPlayerWidget.dart';
+import 'package:video_player/video_player.dart';
 
 class NetworkVideoPlayer extends StatefulWidget {
   @override
@@ -6,8 +11,53 @@ class NetworkVideoPlayer extends StatefulWidget {
 }
 
 class _NetworkVideoPlayerState extends State<NetworkVideoPlayer> {
+  final textController = TextEditingController(text: urlLandscapeVideo);
+  VideoPlayerController networkController;
+
+  @override
+  void initState() {
+    super.initState();
+    networkController = VideoPlayerController.network(textController.text)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..setLooping(true)
+      ..initialize().then((value) => networkController.play());
+  }
+
+  @override
+  void dispose() {
+    networkController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          VideoPlayerWidget(controller: networkController),
+          buildTextField(),
+        ],
+      ),
+    );
   }
+
+  Widget buildTextField() => Container(
+        padding: EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextFieldWidget(controller: textController, hintText: 'Enter Video Url'),
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            FloatingActionButtonWidget(onPressed: () {
+              if (textController.text.trim().isEmpty) return;
+            }),
+          ],
+        ),
+      );
 }
